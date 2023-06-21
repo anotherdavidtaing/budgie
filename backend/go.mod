4f697e990e7903443a1706d336fedb708d2b6e65
davidtaing <adavidtaing@gmail.com>
2023-06-21 21:13:50 +1000

    feat: implement hello world server
    using chi-router and net/http

--- backend/go.mod
+++ backend/go.mod
@@ -5,6 +5,7 @@ go 1.18
 require github.com/clerkinc/clerk-sdk-go v1.43.0
 
 require (
+	github.com/go-chi/chi/v5 v5.0.8
 	github.com/go-jose/go-jose/v3 v3.0.0 // indirect
 	golang.org/x/crypto v0.7.0 // indirect
 )
--- backend/main.go
+++ backend/main.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World"))
+	})
+
+	fmt.Println("Starting http server on PORT :3000")
+
+	err := http.ListenAndServe(":3000", r)
+
+	if err != nil {
+		log.Fatalln("Failed to start server on PORT :3000")
+	}
 }
