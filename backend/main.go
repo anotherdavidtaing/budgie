4f8c2221f2ee3fe2e1df9dc3f7aa82f59ddcfe13
davidtaing <adavidtaing@gmail.com>
2023-06-21 20:13:35 +1000

    feat: implement hello world fiber server
    using github.com/gofiber/fiber

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
-	fmt.Println("hello world")
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	app.Listen(":3000")
 }
