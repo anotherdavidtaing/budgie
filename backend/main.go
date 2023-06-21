6d03ddcf5c63dc0397fcb50bd9f238cbfd4c2d2c
davidtaing <adavidtaing@gmail.com>
2023-06-21 20:51:24 +1000

    feat: remove gofiber
    due to it is being incompatible with the Clerk Golang SDK.
    
    This is due to fiber not using the standard net/http package which
    one of Clerk's dependency

--- backend/main.go
+++ backend/main.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-	"github.com/gofiber/fiber/v2"
+	"fmt"
 )
 
 func main() {
-	app := fiber.New()
-
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
-	})
-
-	app.Listen(":3000")
+	fmt.Println("Hello, World!")
 }
